9ff106e236fc46beeb9dcb5f38494bed6842c230
HasanKazemi <avatarhasan78@gmail.com>
2025-04-18 12:13:16 +0330

    access each project's details inside related page

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'
+import { Project } from '../types';
 
-const TasksPage = () => {
+const TasksPage:React.FC = () => {
+    const params = useParams()
+    const projects:Project[] = useSelector((state:{projects:Project[]}) => state.projects)
+
+    const thisProject = projects.find((project)=> project.id === (Number(params.projectId)))
+
   return (
-    <div>TasksPage</div>
+    <div>
+        <h1>{thisProject?.title}</h1>
+    </div>
   )
 }
 
