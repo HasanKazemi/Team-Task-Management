4999c2579f25b484039d862772857a52e46a7bde
HasanKazemi <avatarhasan78@gmail.com>
2025-04-24 22:28:30 +0330

    generate and decode token

---
+++ src/jwt/mockToken.ts
@@ -0,0 +1,12 @@
+import { User } from "../types";
+
+export const generateToken = (userData: User): string => {
+    const format = `${userData.id}${userData.name}${userData.password}${userData.role}`
+    const token = btoa(JSON.stringify(format))
+    return token
+};
+
+export const decodeToken = (token: string): User => {
+    const decoded = JSON.parse(atob(token))
+    return decoded
+}
