987dc12192e737c5a9c7e0078cbb1705dcf14342
HasanKazemi <avatarhasan78@gmail.com>
2025-04-25 11:51:33 +0330

    show all projects and the tasks of each member

--- src/pages/public/MemberDashboard.tsx
+++ src/pages/public/MemberDashboard.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Project, Task } from '../../types'
+import { AuthState } from '../../redux/slices/AuthSlice'
 
-const MemberDashboard : React.FC = () => {
+const MemberDashboard: React.FC = () => {
+  const {user} = useSelector((state:{auth: AuthState}) => state.auth)
+  const userId = user?.split(".")[0]
+  const projects = useSelector((state: { projects: Project[] }) => state.projects)
+  const allTasks = useSelector((state: { tasks: Task[] }) => state.tasks)
+
+  const myTasks = allTasks.filter(task => task.id === Number(userId))
+  console.log(myTasks);
+
   return (
-    <div>MemberDashboard</div>
+    <div>
+      <div style={{ marginTop: "50px" }}>
+        <h2>Project List</h2>
+        {projects.map(project => (
+          <div key={project.id} style={{ display: "flex", gap: "20px" }}>
+            <p>{project.title}</p>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
+      <div style={{ marginTop: "50px" }}>
+        <h2>My Tasks</h2>
+        {
+          myTasks.map(task => (
+            <div key={task.id} style={{ display: "flex", gap: "20px" }}>
+              <p>{task.title}</p>
+              <p>{task.description}</p>
+              <p>{task.status}</p>
+              <p>{task.priority}</p>
+              <p>{task.deadline}</p>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
